a9787d0c4afb5e479acb7090b06086881e4999bd
sandeep <sandeep.rawat@evontech.com>
2018-12-07 13:54:44 +0530

    changes

--- src/ios/MathCalculator.m
+++ src/ios/MathCalculator.m
@@ -5,8 +5,6 @@
 @interface MathCalculator : CDVPlugin {
   // Member variables go here.
 }
-
-- (void)coolMethod:(CDVInvokedUrlCommand*)command;
 - (void)add:(CDVInvokedUrlCommand*)command;
 @end
 
@@ -16,7 +14,7 @@
 - (void)add:(CDVInvokedUrlCommand*)command{
 
     CDVPluginResult* pluginResult = nil;
-    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:"Success"];
+    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"Success"];
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
--- www/MathCalculator.js
+++ www/MathCalculator.js
@@ -1,9 +1,5 @@
 var exec = require("cordova/exec");
 
-exports.coolMethod = function(arg0, success, error) {
-  exec(success, error, "MathCalculator", "coolMethod", [arg0]);
-};
-
 module.exports.add = function(arg0, success, error) {
   exec(success, error, "MathCalculator", "add", [arg0]);
 };
