b9d24fdf24d14135807171e42b50de13e7f9565c
sandeep <sandeep.rawat@evontech.com>
2019-01-22 14:04:39 +0530

    remove extra line

--- src/ios/MathCalculator.m
+++ src/ios/MathCalculator.m
@@ -25,7 +25,7 @@
     UIBackgroundTaskIdentifier bgTask = UIBackgroundTaskInvalid;
     bgTask = [[UIApplication sharedApplication]
          beginBackgroundTaskWithExpirationHandler:^{
-             [[UIApplication sharedApplication] endBackgroundTask:bgTask];
+              
     }];
    
    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
